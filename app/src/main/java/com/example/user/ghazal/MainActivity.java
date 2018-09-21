c3176293200e5187bdc61841c74f857697044406
user <ghazalnahas2@gmail.com>
2018-09-21 09:49:45 +0300

    option menu

--- app/src/main/java/com/example/user/ghazal/MainActivity.java
+++ app/src/main/java/com/example/user/ghazal/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.user.ghazal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+
+                Toast.makeText(this, "Item menu 1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item2:
+
+                Toast.makeText(this, "Item menu 2", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
